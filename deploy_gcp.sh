2417e9be381bf1947067017084f6020852917b51
joelin <tingjan1982@gmail.com>
2019-06-11 20:34:19 +0800

    deploy to gcp via Travis CI.

---
+++ deploy_gcp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Login using service account: https://cloud.google.com/sdk/gcloud/reference/auth/activate-service-account
+
+gcloud components install kubectl --quiet
+gcloud config set project helpful-range-236813
+gcloud auth activate-service-account travis-ci-user@helpful-range-236813.iam.gserviceaccount.com --key-file helpful-range-236813-681ca732dc15.json
+gcloud config set compute/zone asia-east1-b
+
+#gcloud container clusters create nextpos-cluster --num-nodes=2
+#kubectl run nextpos-web --image=docker.io/joelin/nextpos-service:latest --port 8080
+#kubectl expose deployment nextpos-web --type=LoadBalancer --port 80 --target-port 8080
