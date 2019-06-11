208ef6c32ad420fe9459ceeb6abab02588eb8e63
joelin <tingjan1982@gmail.com>
2019-06-11 22:47:26 +0800

    Test new service version gets deployed to gcp.

--- deploy_gcp.sh
+++ deploy_gcp.sh
@@ -7,8 +7,9 @@ gcloud config set project helpful-range-236813
 gcloud auth activate-service-account travis-ci-user@helpful-range-236813.iam.gserviceaccount.com --key-file helpful-range-236813-681ca732dc15.json
 gcloud config set compute/zone asia-east1-b
 
-#gcloud container clusters create nextpos-cluster --num-nodes=2
+#gcloud container clusters create nextpos-cluster --num-nodes=2 --enable-cloud-logging
 #kubectl run nextpos-web --image=docker.io/joelin/nextpos-service:latest --port 8080
 #kubectl expose deployment nextpos-web --type=LoadBalancer --port 80 --target-port 8080
 
+gcloud container clusters get-credentials nextpos-cluster --zone asia-east1-b --project helpful-range-236813
 kubectl set image deployment/nextpos-web nextpos-web=docker.io/joelin/nextpos-service:latest
---
+++ src/main/java/io/nextpos/ordermanagement/web/OrderController.java
@@ -0,0 +1,15 @@
+package io.nextpos.ordermanagement.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping
+    public String test() {
+        return "Hello Order";
+    }
+}
