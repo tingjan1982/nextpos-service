1375375cda81bed569fe583a4dde94e45173e382
joelin <tingjan1982@gmail.com>
2019-12-27 20:42:44 +0800

    updated offers API

--- src/main/java/io/nextpos/merchandising/web/OfferController.java
+++ src/main/java/io/nextpos/merchandising/web/OfferController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,7 +33,7 @@ public class OfferController {
                             entry.getKey().getDiscountName(),
                             offer.getTriggerType(),
                             offer.getDiscountDetails().getDiscountType(),
-                            offer.getDiscountDetails().getDiscountValue());
+                            offer.getDiscountDetails().getDiscountValue().multiply(BigDecimal.valueOf(100)));
                 }).collect(Collectors.toList());
 
         return new OffersResponse(offers);
