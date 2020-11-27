2652c234cd4b1800e4dc973041a9150916d0ba4f
joelin <tingjan1982@gmail.com>
2020-11-27 11:42:13 +0800

    fixed table id issue.

--- src/main/java/io/nextpos/ordermanagement/web/factory/OrderCreationFactoryImpl.java
+++ src/main/java/io/nextpos/ordermanagement/web/factory/OrderCreationFactoryImpl.java
@@ -19,6 +19,7 @@ import io.nextpos.shared.auth.OAuth2Helper;
 import io.nextpos.shared.exception.BusinessLogicException;
 import io.nextpos.tablelayout.data.TableLayout;
 import io.nextpos.tablelayout.service.TableLayoutService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -96,7 +97,11 @@ public class OrderCreationFactoryImpl implements OrderCreationFactory {
 
         if (orderRequest.getOrderType() == Order.OrderType.IN_STORE) {
             Set<String> tableIds = new HashSet<>(orderRequest.getTableIds());
-            tableIds.add(orderRequest.getTableId());
+
+            // todo: to remove in the future.
+            if (StringUtils.isNotBlank(orderRequest.getTableId())) {
+                tableIds.add(orderRequest.getTableId());
+            }
 
             final List<TableLayout.TableDetails> tables = tableIds.stream()
                     .map(tableLayoutService::getTableDetailsOrThrows)
